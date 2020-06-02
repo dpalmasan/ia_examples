7db1fbea0894d887973d56fbc36324b9115326df
dpalmasan <dpalmasan@gmail.com>
2020-06-02 09:31:49 -0400

    Updates

---
+++ precache-manifest.a81195f816f27a919013112af609b8dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3bad746585a70bc03cb89ebc038518d",
+    "url": "/ia_examples/index.html"
+  },
+  {
+    "revision": "7d0e8d6631d491eb4b25",
+    "url": "/ia_examples/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "07a1a4b4643bdb1a860d",
+    "url": "/ia_examples/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "7d0e8d6631d491eb4b25",
+    "url": "/ia_examples/static/js/2.677593b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ia_examples/static/js/2.677593b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07a1a4b4643bdb1a860d",
+    "url": "/ia_examples/static/js/main.43619fd9.chunk.js"
+  },
+  {
+    "revision": "886caeddf7715d1f75df",
+    "url": "/ia_examples/static/js/runtime-main.54ce7f24.js"
+  }
+]);
